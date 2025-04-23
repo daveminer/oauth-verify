86dc8f87a5f25080a3c000d221b4d525fec1750d
Dave Miner <wtrispsn@gmail.com>
2025-04-23 01:15:09 -0400

    works with local jwt generation script

--- scripts/build-jwt.ts
+++ scripts/build-jwt.ts
@@ -1,6 +1,5 @@
-import { generateKeyPairSync } from "crypto";
+import crypto from "crypto";
 import jsonwebtoken from "jsonwebtoken";
-import sshpk from "sshpk";
 
 import { createRequire } from "module";
 const require = createRequire(import.meta.url);
@@ -9,31 +8,31 @@ const noirJwt = require("noir-jwt");
 const maxSignedDataLength = 900;
 const shaPrecomputeTillKeys = undefined;
 
-const { publicKey, privateKey } = generateKeyPairSync("rsa", {
-  modulusLength: 2048,
-  publicKeyEncoding: {
-    type: "spki",
-    format: "pem",
-  },
-  privateKeyEncoding: {
-    type: "pkcs8",
-    format: "pem",
-  },
-});
+const { publicKey: publicKeyPem, privateKey: privateKeyPem } =
+  crypto.generateKeyPairSync("rsa", {
+    modulusLength: 2048,
+    // publicKeyEncoding: {
+    //   type: "spki",
+    //   format: "pem",
+    // },
+    // privateKeyEncoding: {
+    //   type: "pkcs1",
+    //   format: "pem",
+    // },
+  });
 
-const pubKey = sshpk.parseKey(publicKey, "pem");
-
-const token = jsonwebtoken.sign({ data: "foobar" }, privateKey, {
+const token = jsonwebtoken.sign({ data: "foobar" }, privateKeyPem, {
   algorithm: "RS256",
 });
 
+const jwk = publicKeyPem.export({
+  format: "jwk",
+});
+
+// Now use the JWK format for generateInputs
 const inputs = noirJwt.generateInputs({
   jwt: token,
-  pubkey: {
-    kty: "RSA",
-    n: pubKey.toString("ssh").split(" ")[1], // Extract the base64 encoded modulus
-    e: "AQAB", // Default RSA exponent 65537 in base64
-  },
+  pubkey: jwk,
   maxSignedDataLength,
   shaPrecomputeTillKeys,
 });
